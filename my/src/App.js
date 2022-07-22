8cd83659f7823b0ab2dc048b088ee25713e79340
Elif <elffemre@gmail.com>
2022-07-22 19:59:11 +0300

    useState
    -useState kavramını öğrendik

--- my/src/App.js
+++ my/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [name,setName] = useState("elif");
+    const [age,setAge] = useState(29);
+
+
+    return ( 
+        <div className = "App" >
+
+        <h1>Merhaba {name}</h1>
+        <h2>{age}</h2>
+        
+        <button onClick={() => setName("Ahmet")}>Click</button>
+        <button onClick={() => setAge(25)}>change age</button>
+        </div>
+    );
 }
 
 export default App;
