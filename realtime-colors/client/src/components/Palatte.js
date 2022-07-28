4cca7190c50d0c345147595a8f9f9f5cfac528bc
Elif <elffemre@gmail.com>
2022-07-28 15:58:34 +0300

    arayüz hazırlanması

---
+++ realtime-colors/client/src/components/Palatte.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Palatte() {
+  return (
+    <div className='palatte'>
+
+
+    <input type="color"></input>
+
+    <button>Click</button>
+    
+    
+    
+    </div>
+  )
+}
+
+export default Palatte
