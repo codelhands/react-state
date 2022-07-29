06729362ea0b5c3d126ce84c69060151c3699b6b
Elif <elffemre@gmail.com>
2022-07-29 16:54:41 +0300

    backende bağlandık
    server is up :airplane:

--- realtime-colors/client/src/App.js
+++ realtime-colors/client/src/App.js
@@ -1,7 +1,18 @@
 import './App.css'; 
+
+import { init } from './socketApi';
+
+import { useEffect } from 'react';
+
 import Palatte from './components/Palatte';
 
 function App() {
+
+  useEffect(() => {
+    init();
+  },[]);
+
+
   return (
     <div className="App">
      <Palatte></Palatte>
